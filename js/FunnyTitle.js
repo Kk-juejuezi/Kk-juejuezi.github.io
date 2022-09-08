7b728b6039dd1263cdfc00afe1dc91da6e20394f
Kk-juejuezi <253292942@qq.com>
2022-09-08 15:50:48 +0800

    Site updated: 2022-09-08 15:50:48

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        document.title = '(°A°`)页面崩溃啦';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫你回来了 ~' + OriginTitle;
+        document.title = '(ฅ>ω<*ฅ)噫你回来了' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
